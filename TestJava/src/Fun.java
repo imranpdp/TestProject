5a1553c532114557f0b1c5b2deffa1d0845a34bf
Imran Ali <Imran Ali@MDIALI-LAP.oradev.oraclecorp.com>
2016-12-04 05:04:39 +0530

    Imran: Real fun begin

--- TestJava/src/Fun.java
+++ TestJava/src/Fun.java
@@ -14,6 +14,7 @@ public class Fun {
 	public static void main(String[] args) {
 		System.out.println("Hi how are");
 		System.out.println("it worked for me as well");
+		System.out.println("This is working man Great keep that up");
 	}
 
 }
